04582023701f6df68c2b0fcd20ecea0e5e29e5eb
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-09-23 12:53:53 +0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// conventional props
+function Heading({ title }: { title: string }) {
+  return <h1>{ title }</h1>
+}
+function HeadingWithContent({ children }: { children: string }) {
+  return <h1>{ children }</h1>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading title='Hello'></Heading>
+      <HeadingWithContent>Hi!</HeadingWithContent>
     </div>
   );
 }
